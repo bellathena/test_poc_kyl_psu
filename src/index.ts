9da259d4032cab517669f86a84e5421f7f2fd4db
bellathena <thanaporn.jin04@gmail.com>
2026-06-14 23:20:41 +0700

    add CORS support to Elysia app and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Elysia } from "elysia";
 import { openapi } from "@elysia/openapi";
 import { routes } from "./api/app.route";
+import { cors } from "@elysia/cors";
 
 const app = new Elysia()
+  .use(cors(
+    {
+      origin: "http://localhost:5174",
+    }
+  ))
   .use(openapi())
   .use(routes)
   .listen(5000);
